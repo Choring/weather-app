4eda0d435cf7ee9620cfcbe20811c4cba656fc91
조인철 <skyhonor201@gmail.com>
2024-08-17 17:36:47 +0900

    과제 추가

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ import ClipLoader from "react-spinners/ClipLoader";
 function App() {
   const [weather, setWeather] = useState(null);
   const cities = ["paris", "new york", "tokyo", "seoul"];
-  const [city, setCity] = useState("");
-  let [loading, setLoading] = useState(true);
-  
+  const [city, setCity] = useState("current");
+  const [loading, setLoading] = useState(true);
+  let [handleError, setHandleError] = useState("");
+
   const getCurrentLocation= () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       let lat = position.coords.latitude;
@@ -28,39 +29,53 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async(lat,lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0bedb45c75711fa3349b7114b5743974&units=metric`;
-    setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    setWeather(data);
-    setLoading(false);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0bedb45c75711fa3349b7114b5743974&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setHandleError(error);
+      setLoading(false);
+    }
+    
   }
 
   const getWeatherByCity = async() => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bedb45c75711fa3349b7114b5743974&units=metric`;
-    setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    setWeather(data);
-    setLoading(false);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bedb45c75711fa3349b7114b5743974&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setHandleError(error);
+      setLoading(false);
+    }
+    
   }
 
   useEffect(() => {
-    city === "" ? getCurrentLocation() : getWeatherByCity();
+    city === "current" ? getCurrentLocation() : getWeatherByCity();
   },[city]);
 
   return (
     <>
+    {(handleError == "" ) ? 
       <div className="container">
         {loading ? <ClipLoader
-        color={"#f88c6b"}
-        loading={loading}
-        size={150}
-        /> :
-        <WeatherBox weather={weather} />
+          color={"#f88c6b"}
+          loading={loading}
+          size={150}
+          /> :
+          <WeatherBox weather={weather} />
         }
         <WeatherButton cities={cities} setCity={setCity} />
       </div>
+    : <h3>{handleError}</h3>}
     </>
   );
 }
--- src/components/WeatherButton.js
+++ src/components/WeatherButton.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import {Button} from "react-bootstrap";
 import ButtonContainer from '../style/ButtonContainer';
 
 const WeatherButton = ({cities, setCity}) => {
+  const [active, setActive] = useState("current");
+
+  const handleClick = (e) => {
+    e.target.value === "current" ? setCity("current") : setCity(e.target.value);
+    setActive(() => {
+      return e.target.value
+    })
+    
+  }
 
   return (
     <ButtonContainer>
-        <Button variant="warning">Current Location</Button>
+        <Button variant={active === "current" ? "danger" : "warning"} onClick={handleClick} value="current">Current Location</Button>
         {cities.map((item, index) => (
-          <Button variant="warning" key={index} onClick={() => setCity(item)}>{item}</Button>
+          <Button variant={item === active ? "danger" : "warning"} key={index} onClick={handleClick} value={item}>{item}</Button>
         ))}
     </ButtonContainer>
   )
